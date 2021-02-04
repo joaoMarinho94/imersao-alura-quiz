a4a1483861b2c3ae195935f48388728da88cea26
joaoMarinho94 <joaopaulim94@gmail.com>
2021-02-04 17:17:09 -0300

    Lint all files

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
-import styled from 'styled-components'
-import db from '../db.json'
+import React from 'react';
+import styled from 'styled-components';
+import db from '../db.json';
 
-import Widget from '../src/components/Widget'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
-import QuizBackground from '../src/components/QuizBackground'
-import QuizLogo from '../src/components/QuizLogo'
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizLogo from '../src/components/QuizLogo';
 
 const QuizContainer = styled.div`
   width: 100%;
@@ -22,18 +23,18 @@ export default function Home() {
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
-        <QuizLogo/>
+        <QuizLogo />
         <Widget>
-        <Widget.Header>
-          <h1>teste</h1>
-        </Widget.Header>
-        <Widget.Content>
-          <p>dasdasdasdasdasd asdasd asdasd asdasdasd</p>
-        </Widget.Content>
+          <Widget.Header>
+            <h1>teste</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>dasdasdasdasdasd asdasd asdasd asdasdasd</p>
+          </Widget.Content>
         </Widget>
         <Footer />
       </QuizContainer>
       <GitHubCorner projectUrl="https://github.com/joaoMarinho94" />
     </QuizBackground>
-  )
+  );
 }
